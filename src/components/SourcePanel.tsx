8eae82bb906fc156944316712c9a4645125ba59b
kmdtomo <tomo.wsf11@gmail.com>
2025-08-18 20:52:41 +0900

    add:ビルドエラー対処

--- src/components/SourcePanel.tsx
+++ src/components/SourcePanel.tsx
@@ -50,7 +50,7 @@ export function SourcePanel({ sources, selectedSourceIndex, onClose }: SourcePan
         {sources.map((source, index) => (
           <div
             key={index}
-            ref={el => sourceRefs.current[index] = el}
+            ref={el => { sourceRefs.current[index] = el; }}
             id={`source-${index}`}
             className={`border rounded-lg p-4 transition-all ${
               selectedSourceIndex === index 
